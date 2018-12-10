e8b04f8e53d3f75147bd3c3fb00a035cb2487de7
ZwiebelnX <sicong.chen@163.com>
2018-12-10 22:35:15 +0800

    Initial commit

---
+++ src/main/java/com/spm8/goodgoodstudyserver/GoodgoodstudyServerApplication.java
@@ -0,0 +1,12 @@
+package com.spm8.goodgoodstudyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodgoodstudyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodgoodstudyServerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/spm8/goodgoodstudyserver/GoodgoodstudyServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spm8.goodgoodstudyserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GoodgoodstudyServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
